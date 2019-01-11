7ead82b3aec69cc5d9c844e5b557ea84e3312d1c
Jensen94 <jensen.steven94@gmail.com>
2019-01-10 22:14:07 -0500

    Updated Interface with Robinhood API. Still working out bugs with prices.

---
+++ Interface.py
@@ -0,0 +1,33 @@
+#Interface Code with Robinhood.py. Call upon functions within Robinhood.py in order to interface with the website
+#and log in. This code will eventually connect with analysis tool in order to determine whether to buy or sell.
+
+import json
+from Robinhood import Robinhood
+
+#Assigning Crypto Currencies we will be researching
+Cryptos =  ["ETH","LTC","BCH","QTUM","BTG","NEO"]
+Cryptos_name = ["Ethereum", "Litecoin", "Bitcoin Cash", "Qtum", "Bitcoin Gold", "NEO"]
+Crypto_length = len(Cryptos)
+Cryptos_price = [0] * Crypto_length
+Crypto_DataFetch = [0] * Crypto_length
+
+#Logging into Robinhood and initializin Crypto Currency searches
+
+login_instance = Robinhood("username","password")
+
+counter = 0
+while counter < Crypto_length :
+	#Crypto_DataFetch[counter] = login_instance.instruments(Cryptos[counter])
+	#print Crypto_DataFetch[counter]
+	Cryptos_price[counter] = login_instance.quote_data(Cryptos[counter])
+	Crypto_DataFetch[counter] = json.loads(Cryptos_price[counter].text)
+	export1 = json.dumps(Crypto_DataFetch[counter], sort_keys=True, indent=4)
+	print Crypto_DataFetch[counter]
+	counter += 1
+	json.loads(requester1.text)
+	export1 = json.dumps(data1, sort_keys=True, indent=4)
+#Printing function for information requested.
+counter = 0
+while counter < Crypto_length :
+	print Cryptos_name[counter] + Crypto_DataFetch[counter]
+	counter += 1
--- Robinhood.py
+++ Robinhood.py
@@ -3,102 +3,104 @@ import urllib
 
 class Robinhood:
 
-    endpoints = {
-            "login": "https://api.robinhood.com/api-token-auth/",
-            "investment_profile": "https://api.robinhood.com/user/investment_profile/",
-            "accounts":"https://api.robinhood.com/accounts/",
-            "ach_iav_auth":"https://api.robinhood.com/ach/iav/auth/",
-            "ach_relationships":"https://api.robinhood.com/ach/relationships/",
-            "ach_transfers":"https://api.robinhood.com/ach/transfers/",
-            "applications":"https://api.robinhood.com/applications/",
-            "dividends":"https://api.robinhood.com/dividends/",
-            "edocuments":"https://api.robinhood.com/documents/",
-            "instruments":"https://api.robinhood.com/instruments/",
-            "margin_upgrades":"https://api.robinhood.com/margin/upgrades/",
-            "markets":"https://api.robinhood.com/markets/",
-            "notifications":"https://api.robinhood.com/notifications/",
-            "orders":"https://api.robinhood.com/orders/",
-            "password_reset":"https://api.robinhood.com/password_reset/request/",
-            "quotes":"https://api.robinhood.com/quotes/",
-            "document_requests":"https://api.robinhood.com/upload/document_requests/",
-            "user":"https://api.robinhood.com/user/",
-            "watchlists":"https://api.robinhood.com/watchlists/",
-    }
+	endpoints = {
+			"login": "https://api.robinhood.com/oauth2/token/",
+			"investment_profile": "https://api.robinhood.com/user/investment_profile/",
+			"accounts":"https://api.robinhood.com/accounts/",
+			"ach_iav_auth":"https://api.robinhood.com/ach/iav/auth/",
+			"ach_relationships":"https://api.robinhood.com/ach/relationships/",
+			"ach_transfers":"https://api.robinhood.com/ach/transfers/",
+			"applications":"https://api.robinhood.com/applications/",
+			"dividends":"https://api.robinhood.com/dividends/",
+			"edocuments":"https://api.robinhood.com/documents/",
+			"instruments":"https://api.robinhood.com/instruments/",
+			"margin_upgrades":"https://api.robinhood.com/margin/upgrades/",
+			"markets":"https://api.robinhood.com/markets/",
+			"notifications":"https://api.robinhood.com/notifications/",
+			"orders":"https://api.robinhood.com/orders/",
+			"password_reset":"https://api.robinhood.com/password_reset/request/",
+			"quotes":"https://api.robinhood.com/quotes/",
+			"document_requests":"https://api.robinhood.com/upload/document_requests/",
+			"user":"https://api.robinhood.com/user/",
+			"watchlists":"https://api.robinhood.com/watchlists/",
+	}
 
-    session = None
+	session = "None"
 
-    username = None
+	username = "None"
 
-    password = None
+	password = "None"
 
-    headers = None
+	headers = "None"
 
-    auth_token = None
+	auth_token = "None"
 
-    account_url = None
+	account_url = "None"
 
-    def __init__(self, username, password):
-        self.session = requests.session()
-        self.session.proxies = urllib.getproxies()
-        self.username = username
-        self.password = password
-        self.headers = {
-            "Accept": "*/*",
-            "Accept-Encoding": "gzip, deflate",
-            "Accept-Language": "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5",
-            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
-            "X-Robinhood-API-Version": "1.0.0",
-            "Connection": "keep-alive",
-            "User-Agent": "Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)"
-        }
-        
-        self.session.headers = self.headers
-        self.login()
-        
-        ## set account url
-        acc = self.get_account_number()
-        self.account_url = self.endpoints['accounts'] + acc + "/"
+	def __init__(self, username, password):
+		self.session = requests.session()
+		self.session.proxies = urllib.getproxies()
+		self.username = username
+		self.password = password
+		self.grant_type = "password"
+		self.client_id = "c82SH0WZOsabOXGP2sxqcj34FxkvfnWRZBKlBjFS"
+		self.headers = {
+			"Accept": "*/*",
+			"Accept-Encoding": "gzip, deflate",
+			"Accept-Language": "en;q=1, fr;q=0.9, de;q=0.8, ja;q=0.7, nl;q=0.6, it;q=0.5",
+			"Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+			"X-Robinhood-API-Version": "1.0.0",
+			"Connection": "keep-alive",
+			"User-Agent": "Robinhood/823 (iPhone; iOS 7.1.2; Scale/2.00)"
+		}
+		
+		self.session.headers = self.headers
+		self.login()
 
- 
-    def login(self):
-        data = "password=%s&username=%s" % (self.password, self.username)
-        res = self.session.post(self.endpoints['login'], data=data)
-        res = res.json()
-        self.auth_token = res['token']
-        self.headers['Authorization'] = 'Token '+self.auth_token
+		## set account url
+		acc = self.get_account_number()
+		self.account_url = self.endpoints['accounts'] + acc + "/"
 
-    def get_account_number(self):
-        res = self.session.get(self.endpoints['ach_relationships'])
-        res =  res.json()['results'][0]
-        account_number = res['account'].split('accounts/', 1)[1][:-1]
-        print account_number
-        return account_number
 
-    def investment_profile(self):
-        self.session.get(self.endpoints['investment_profile'])
+	def login(self):
+		data = "password=%s&username=%s&grant_type=%s&client_id=%s" % (self.password, self.username, self.grant_type, self.client_id)
+		res = self.session.post(self.endpoints['login'], data=data)
+		res = res.json()
+		self.oauth_token = res['access_token']
+		self.headers['Authorization'] = 'Bearer '+self.oauth_token
 
-    def instruments(self, stock=None):
-        res = self.session.get(self.endpoints['instruments'], params={'query':stock.upper()})
-        res = res.json()
-        return res['results']
+	def get_account_number(self):
+		res = self.session.get(self.endpoints['ach_relationships'])
+		res = res.json()['results'][0]
+		account_number = res['account'].split('accounts/', 1)[1][:-1]
+		print account_number
+		return account_number
 
-    def quote_data(self, stock):
-        params = { 'symbols': stock }
-        res = self.session.get(self.endpoints['quotes'], params=params)
-        res = res.json()
-        return res['results']
+	def investment_profile(self):
+		self.session.get(self.endpoints['investment_profile'])
 
-    def place_order(self, instrument, quantity=1, bid_price = None, transaction=None):
-        if bid_price == None:
-            bid_price = self.quote_data(instrument['symbol'])[0]['bid_price']
-        data = 'account=%s&instrument=%s&price=%f&quantity=%d&side=%s&symbol=%s&time_in_force=gfd&trigger=immediate&type=market' % (urllib.quote(self.account_url), urllib.unquote(instrument['url']), float(bid_price), quantity, transaction, instrument['symbol']) 
-        res = self.session.post(self.endpoints['orders'], data=data)
-        return res
+	def instruments(self, stock):
+		res = self.session.get(self.endpoints['instruments'], params={'query':stock.upper()})
+		res = res.json()
+		return res['results']
 
-    def place_buy_order(self, instrument, quantity, bid_price=None):
-        transaction = "buy"
-        return self.place_order(instrument, quantity, bid_price, transaction)
+	def quote_data(self, stock):
+		params = { 'symbols': stock }
+		res = self.session.get(self.endpoints['quotes'], params=params)
+		res = res.json()
+		return res['results']
 
-    def place_sell_order(self, instrument, quantity, bid_price=None):
-        transaction = "sell"
-        return self.place_order(instrument, quantity, bid_price, transaction)
+	def place_order(self, instrument, quantity=1, bid_price = None, transaction=None):
+		if bid_price == None:
+			bid_price = self.quote_data(instrument['symbol'])[0]['bid_price']
+		data = 'account=%s&instrument=%s&price=%f&quantity=%d&side=%s&symbol=%s&time_in_force=gfd&trigger=immediate&type=market' % (urllib.quote(self.account_url), urllib.unquote(instrument['url']), float(bid_price), quantity, transaction, instrument['symbol'])
+		res = self.session.post(self.endpoints['orders'], data=data)
+		return res
+
+	def place_buy_order(self, instrument, quantity, bid_price=None):
+		transaction = "buy"
+		return self.place_order(instrument, quantity, bid_price, transaction)
+
+	def place_sell_order(self, instrument, quantity, bid_price=None):
+		transaction = "sell"
+		return self.place_order(instrument, quantity, bid_price, transaction)
